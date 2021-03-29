97db1b974f05907bc6d36fbd42e563c3cc17e55b
HarunAGCA <harun39117@gmail.com>
2021-03-29 15:05:48 +0300

    Added sonnection string provider tool

---
+++ Core/DataAccess/ConnectionStringProviders/ConnectionStringTool.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.DataAccess.ConnectionStringProviders
+{
+    public class ConnectionStringTool
+    {
+
+        public static string GetConnectionString(string environmentVariableName)
+        {
+            var connectionString = Environment.GetEnvironmentVariable(environmentVariableName);
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new Exception("There is no valid connection string environment variable");
+            }
+
+            return connectionString;
+        }
+
+        public static string GetConnectionStringFromHerokuContainer()
+        {
+            string connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+            var databaseUri = new Uri(connectionUrl);
+
+            string db = databaseUri.LocalPath.TrimStart('/');
+            string[] userInfo = databaseUri.UserInfo.Split(':', (char)StringSplitOptions.RemoveEmptyEntries);
+
+            return $"User ID={userInfo[0]};Password={userInfo[1]};Host={databaseUri.Host};Port={databaseUri.Port};Database={db};Pooling=true;SSL Mode=Require;Trust Server Certificate=True;";
+
+        }
+
+        public static string GetConnectionStringDevelopment()
+        {
+            return @"Server=(localdb)\MSSQLLocalDB; Database=CarRental; Integrated Security=true;";
+        }
+    }
+}
+    
